d6c2ea1a6221c051ea3236e2d82d51e04812cbb7
Avalanche <albecker@hotmail.de>
2025-03-12 17:41:22 +0100

    added Traditional draft and sidebar layout

--- 01_SimulationFront.py
+++ 01_SimulationFront.py
@@ -4,29 +4,40 @@ import Simulation
 
 st.title("Draft Simulation")
 
-selection = st.pills("", ["Quickdraft", "Premierdraft"], selection_mode="single")
+selection = st.sidebar.pills("", ["Quickdraft", "Premierdraft", "Traditional"], selection_mode="single", default="Quickdraft")
 if selection == "Quickdraft":
     DraftObject = Simulation.Quickdraft()
 else:
     DraftObject = Simulation.Premierdraft()
 
-winrate = st.number_input("Winrate", value=0.55, min_value=0.0, max_value=1.0)
-number_of_drafts = st.number_input("Number of Drafts", value=1, min_value=1, max_value=9000)
-starting_diamonds = st.number_input("Starting Diamonds", value=10000, min_value=0, max_value=1000000)
+winrate = st.sidebar.number_input("Winrate", value=0.55, min_value=0.0, max_value=1.0)
+number_of_drafts = st.sidebar.number_input("Number of Drafts", value=1, min_value=1, max_value=9000)
+starting_diamonds = st.sidebar.number_input("Starting Diamonds", value=10000, min_value=0, max_value=1000000)
 Simulation.Diamonds = starting_diamonds
+
+
+
 
 #------------------- Simulion start --------------------
 
-
 buttonval = st.button("Start Simulation")
 diamondlist = [Simulation.Diamonds]
+drafts_played = number_of_drafts
 if buttonval:
     for x in range(number_of_drafts):
-        Simulation.start_simulation(DraftObject, winrate)
+        draft_bool = Simulation.draft_simulation(DraftObject, winrate)
         diamondlist.append(Simulation.Diamonds)
-        if Simulation.Diamonds <= 0:
+        
+        if not draft_bool:
+            drafts_played = x
             break
 
-st.line_chart(diamondlist)
-st.write("Diamonds: ", Simulation.Diamonds)
+    st.write("Drafts played: ", drafts_played)
+    st.write("Diamonds after the simulation: ", Simulation.Diamonds)
+    st.line_chart(diamondlist)
+    
+
+
+
+
 
--- Simulation.py
+++ Simulation.py
@@ -14,23 +14,64 @@ class Premierdraft(Draft):
     cost = 1500
     reward = {0:50, 1: 100, 2:250, 3:1000, 4:1400, 5:1600, 6:1800, 7:2200}
 
+class Traditional(Draft):
+    cost = 1500
+    reward = {0:100, 1: 250, 2:1000, 3:2500}
+
 def start_game(winrate)->bool:
-    # get a 55% chance win to be true
     win = np.random.choice([True, False], p=[winrate, 1-winrate])
     return win
 
-def start_simulation(Draft, winrate):
+def draft_simulation(Draft, winrate)->bool:
+    # Simulates an Arena Drafts and updates the Diamond count, based on the winrate
     try:
         Draft.cost
         Draft.reward
     except:
         print("Draft class has no cost")
 
+    # check if Draft is of tyoe traditional
+    if isinstance(Draft, Traditional):
+        sucess = simulate_traditional(Draft, winrate)
+    else:
+        sucess = simulate_quick_and_premier(Draft, winrate)
+
+    return sucess
+
+
+
+def simulate_traditional(Draft, winrate)->bool:
     global Diamonds
+    if Diamonds < Draft.cost:
+        return False
     Diamonds = Diamonds - Draft.cost
     wins = 0
-    looses = 0
+    outcome = {"wins":0, "looses":0}
 
+    for games in range(3):
+        for subgames in range(3):
+            if start_game(winrate):
+                outcome["wins"] += 1
+            else:
+                outcome["looses"] += 1
+
+            if outcome["wins"] == 2:
+                wins += 1
+                break
+            if outcome["looses"] == 2:
+                break
+
+    Diamonds += Draft.reward[wins]
+    return True
+            
+def simulate_quick_and_premier(Draft, winrate)->bool:
+    global Diamonds
+    if Diamonds < Draft.cost:
+        return False
+    Diamonds = Diamonds - Draft.cost
+
+    wins = 0
+    looses = 0
     for game in range(10):
         if start_game(winrate):
             wins += 1
@@ -41,6 +82,4 @@ def start_simulation(Draft, winrate):
             Diamonds += rewards
             wins, looses = 0, 0 
             break
-
-
-  
+    return True
